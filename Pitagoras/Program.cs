f89f3048bc6971e9606c5b4ea9b45f856a41d6e3
Yasmamoto <yamamotoyato31@gmail.com>
2024-03-01 13:57:36 -0400

    Atividade Pitagoras

---
+++ Pitagoras/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+class Pitagoras
+{   static double CalcularPitagoras(double catet1, double catet2)
+    {
+        catet1 = Math.Pow(catet1, 2);
+        catet2 = Math.Pow(catet2, 2);
+
+        double hipotenusa = Math.Sqrt(catet1 + catet2);
+
+        return hipotenusa;
+    }
+    static void menuzinho()
+    {
+        Console.WriteLine(" ----------------------");
+        Console.WriteLine("|                      |");
+        Console.WriteLine("| TEOREMA DE PITAGORAS |");
+        Console.WriteLine("|                      |");
+        Console.WriteLine(" ----------------------");
+    }
+    static void Main(string[] args)
+    {
+        menuzinho();
+        Console.Write("Digite o primeiro cateto: ");
+        double catet1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Digite o segundo cateto: ");
+        double catet2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine($"\nO resultado do teorema de pitagoras deu {CalcularPitagoras(catet1, catet2).ToString("0.00")}");
+
+        Console.ReadKey();
+    }
+}
